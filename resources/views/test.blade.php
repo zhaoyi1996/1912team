6e5afefcff6f2160cca0284a830910e6ea7cf19c
heyuhao <ranhaogg@qq.com>
2020-09-10 15:50:02 +0800

    test.blade.php.注释

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,9 +1,11 @@
- @extends("layouts.indexshop")
+ @extends("layouts.shop")
      @section("title",'商品列表')
+     <!--  -->
      @section('content')
    
+   <!-- 代码部分 -->
 <center>
 	<b>测试模板布局</b>
 </center>	
-
+	<!-- 代码结尾 -->
  @endsection
