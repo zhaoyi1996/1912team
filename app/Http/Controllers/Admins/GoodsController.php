fa4195fc5e5fced73c67bc7749e3ca35d6f06f48
lixuetao <1651928858@qq.com>
2020-09-22 10:10:00 +0800

    修复bug

--- app/Http/Controllers/Admins/GoodsController.php
+++ app/Http/Controllers/Admins/GoodsController.php
@@ -130,8 +130,7 @@ static $info=[];
 
 //    ajax接收值执行添加
     public function img(Request $request){
-        $data=$request->all();
-
+        $data=$request->except('_token');
 //        添加时间
         $data['goods_add_time']=time();
         $data['goods_imgs']=implode('|',$data['goods_imgs']);
