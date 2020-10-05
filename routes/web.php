8907a9cb9225596e335f2a9d2946e218e3e9027d
zhaoyi <1113252039@qq.com>
2020-10-05 13:58:37 +0800

    .

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 //测试
 Route::get("/test",function(){
 	return view("admins.home.index");
@@ -418,3 +416,9 @@ Route::get('/cart/delete/{goods_id}','Index\CartController@delete');
 Route::get('/cart/deletes/{goods_id}','Index\CartController@deletes');
 
 Route::get('/indexs/carts','Index\CartController@carts');
+
+
+// Route::get('/', function () {
+// 	phpinfo();
+//     // return view('welcome');
+// });
